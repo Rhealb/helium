857fdc256b839ac379f78ec8561e41c695175e47
Rhealb <47973244+Rhealb@users.noreply.github.com>
2019-03-21 16:49:07 +0800

    Update no-boardmachine-ansible-docker.sh

--- k8s-ansible-script/no-boardmachine-ansible-docker.sh
+++ k8s-ansible-script/no-boardmachine-ansible-docker.sh
@@ -32,8 +32,7 @@ function install_packages() {
   yum update
   yum remove docker  docker-common docker-selinux docker-engine
   yum install -y yum-utils device-mapper-persistent-data lvm2
-  yum-config-manager --add-repo 
-  https://download.docker.com/linux/centos/docker-ce.repo
+  yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
   yum install -y docker-ce-18.06.0.ce-3.el7 bridge-utils
 
 }
